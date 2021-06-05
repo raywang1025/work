256590176837703fcced2a4164b4ee5972c79f16
Ray Wang <raydingwang@gmail.com>
2021-06-05 22:17:59 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
-$('#toggle').click(function() {
-    $(this).toggleClass('active');
-    $('#overlay').toggleClass('open');
-});
+$(document).ready(function() {
+    // 先把.fade隱藏,延遲0.5秒之後在漸入3秒鐘
+    $(".fade").hide(0).delay(500).fadeIn(3000);
 
-$(function() {
     // 找到.nav裡面的a元素,點擊之後從html跟body中移到,點擊的元素中的href處，用2秒的速度
     $('.nav').find('a').click(function() {
         var $href = $(this).attr('href');
@@ -12,4 +10,4 @@ $(function() {
         }, 2000);
         return false;
     });
-})
+});
