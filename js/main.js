6d57e12dfe3255ed911d09c42c84fc103d6e263d
Ray Wang <raydingwang@gmail.com>
2021-06-05 16:20:04 +0800

    update

--- js/main.js
+++ js/main.js
@@ -2,3 +2,14 @@ $('#toggle').click(function() {
     $(this).toggleClass('active');
     $('#overlay').toggleClass('open');
 });
+
+$(function() {
+    // 找到.nav裡面的a元素,點擊之後從html跟body中移到,點擊的元素中的href處，用2秒的速度
+    $('.nav').find('a').click(function() {
+        var $href = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $($href).offset().top
+        }, 2000);
+        return false;
+    });
+})
