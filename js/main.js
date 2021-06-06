7f66b8d635bbe94fa2dd63c496242091eadcb0b9
Ray Wang <raydingwang@gmail.com>
2021-06-06 12:35:23 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     // 先把.fade隱藏,延遲0.5秒之後在漸入3秒鐘
-    $(".fade").hide(1000).delay(500).fadeIn(3000);
+    $(".fade").hide(0).delay(500).fadeIn(3000);
 
     // 找到.nav裡面的a元素,點擊之後從html跟body中移到,點擊的元素中的href處，用2秒的速度
     $('.nav').find('a').click(function() {
@@ -9,5 +9,17 @@ $(document).ready(function() {
             scrollTop: $($href).offset().top
         }, 2000);
         return false;
+    });
+
+    $(function() {
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 50) {
+                $('.nav').addClass("sticky");
+                $('.nav a').addClass("fixFont");
+            } else {
+                $('.nav').removeClass("sticky");
+                $('.nav a').removeClass("fixFont");
+            }
+        });
     });
 });
